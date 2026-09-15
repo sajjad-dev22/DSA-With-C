f07a0338f215182613882ed1365fb2290bd7fd20
Md Sajjad <mdsajjadalam72950@gmail.com>
2026-09-15 21:04:15 +0530

    Practice 3 Types  Sorting Algorithm

---
+++ SortingAlgo/bubbleSort.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter the elements of the array: ");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    printf("The sorted array is: ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
---
+++ SortingAlgo/selectionSort.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the Size of array:");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter the Array Elements:");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    for (int i = 0; i < n - 1; i++)
+    {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[minIndex])
+            {
+                minIndex = j;
+            }
+        }
+
+        if (minIndex != i)
+        {
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
